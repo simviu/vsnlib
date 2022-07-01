b16040285ed53288d88a07679e729b4003b20d5d
Sherman Chen <schen@simviu.com>
2022-07-01 12:01:28 +0800

    [cv]: add calibration

---
+++ calib/run.sh
@@ -0,0 +1,8 @@
+cd build
+#   ./calib  -w=6 -h=6 scl=0.5 -o=cam.yml -n=5 -d=200 
+
+#   ./calib  -w=6 -h=6 scl=0.25-o=cam.yml -n=5 -d=100 
+#   ./calib  -w=6 -h=6 -o=cam.yml imgs.xml
+
+   ./calib  -w=4 -h=4 -o=cam.yml -n=5 imgs_iphone/para_imgs.xml
+  # ./calib  -w=3 -h=3 -o=cam.yml -n=5 imgs_us/para_imgs.xml
