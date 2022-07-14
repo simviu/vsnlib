616698b6c2f27d09b1f6e430593bd35bc108c087
Sherman Chen <schen@simviu.com>
2022-07-14 17:34:26 +0800

    [aruco]: marker generated with boarder

--- py/aruco/aruco_gen.py
+++ py/aruco/aruco_gen.py
@@ -1,10 +1,13 @@
 import cv2 as cv
 import numpy as np
 import os
+from PIL import Image, ImageOps
 
 K_pathw = "4x4_100/"
+K_pathwb = "4x4_100_b/"
 K_dict = cv.aruco.DICT_4X4_100
-
+WM = 240 # marker width
+BD = 40  # boader size
 #---------
 # gen_marker
 #---------
@@ -13,15 +16,24 @@ def gen_marker(idx):
     dictionary = cv.aruco.Dictionary_get(K_dict)
 
     # Generate the marker
-    markerImage = np.zeros((200, 200), dtype=np.uint8)
-    markerImage = cv.aruco.drawMarker(dictionary, idx, 200, markerImage, 1);
-    pathw = K_pathw
-    if not os.path.exists(pathw):
-        os.mkdir(pathw)
+    im = np.zeros((WM, WM), dtype=np.uint8)
+    im = cv.aruco.drawMarker(dictionary, idx, WM, im, 1);
 
-    sfw = os.path.join(pathw, str(idx)+".png")
-    print("sfw:"+sfw)
-    cv.imwrite(sfw, markerImage);
+    #--- mkdir
+    if not os.path.exists(K_pathw):
+        os.mkdir(K_pathw)
+    if not os.path.exists(K_pathwb):
+        os.mkdir(K_pathwb)
+    #---- aruco marker origin
+    sfw = os.path.join(K_pathw, str(idx)+".png")
+    print("save:"+sfw)
+    cv.imwrite(sfw, im);
+    #---- img add boarder
+    imw = Image.open(sfw)
+    bimg = ImageOps.expand(imw, border=BD, fill="white")
+    sfwb = os.path.join(K_pathwb, str(idx)+".png")
+    print("save:"+sfwb)
+    bimg.save(sfwb)
 
 #----------
 # main
