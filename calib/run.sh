4a7727667ef2d1d1944993da8890b0f895920407
Sherman Chen <schen@simviu.com>
2022-08-09 19:01:50 +0800

    slightly

--- calib/run.sh
+++ calib/run.sh
@@ -4,5 +4,5 @@ cd build
 #   ./calib  -w=6 -h=6 scl=0.25-o=cam.yml -n=5 -d=100 
 #   ./calib  -w=6 -h=6 -o=cam.yml imgs.xml
 
-   ./calib  -w=4 -h=4 -o=cam.yml -n=5 imgs/para_imgs.xml
+   ./calib  -w=4 -h=4 -o=imgs/cam.yml -n=5 imgs/para_imgs.xml
 
