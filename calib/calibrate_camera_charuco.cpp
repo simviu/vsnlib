c616f7ba0c3881a3bc405078e2c2e6c008fb1af0
Sherman Chen <schen@simviu.com>
2022-08-14 21:29:11 +0800

    [calib]: add calib of charuco.

--- calib/calibrate_camera_charuco.cpp
+++ calib/calibrate_camera_charuco.cpp
@@ -102,10 +102,10 @@ int main(int argc, char *argv[]) {
     if(parser.get<bool>("zt")) calibrationFlags |= CALIB_ZERO_TANGENT_DIST;
     if(parser.get<bool>("pc")) calibrationFlags |= CALIB_FIX_PRINCIPAL_POINT;
 
-    Ptr<aruco::DetectorParameters> detectorParams = aruco::DetectorParameters::create();
+    Ptr<aruco::DetectorParameters> detectorParams;
     if(parser.has("dp")) {
         FileStorage fs(parser.get<string>("dp"), FileStorage::READ);
-        bool readOk = detectorParams->readDetectorParameters(fs.root());
+        bool readOk = aruco::DetectorParameters::readDetectorParameters(fs.root(), detectorParams);
         if(!readOk) {
             cerr << "Invalid detector parameters file" << endl;
             return 0;
@@ -135,14 +135,14 @@ int main(int argc, char *argv[]) {
         waitTime = 10;
     }
 
-    Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(0);
+    Ptr<aruco::Dictionary> dictionary;
     if (parser.has("d")) {
         int dictionaryId = parser.get<int>("d");
         dictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
     }
     else if (parser.has("cd")) {
         FileStorage fs(parser.get<std::string>("cd"), FileStorage::READ);
-        bool readOk = dictionary->aruco::Dictionary::readDictionary(fs.root());
+        bool readOk = aruco::Dictionary::readDictionary(fs.root(), dictionary);
         if(!readOk) {
             cerr << "Invalid dictionary file" << endl;
             return 0;
