320f38d3667a2cc71769da11b3e0ff543f37ee96
Sherman Chen <schen@simviu.com>
2023-02-17 15:03:05 +0800

    slightly

--- inc/vsn/vsnLibCv.h
+++ inc/vsn/vsnLibCv.h
@@ -98,6 +98,8 @@ namespace vsn
     protected:
     };
     //---- cast utils
+    cv::Mat img2cv(const Img& im)
+    { ImgCv imc(im); return imc.im_; }
     //------------
     // VideoCv
     //------------
